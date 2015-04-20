d593077ccdb1bed86930ed3ea6ef99e8a2bf3b8b
Santiago Rojo <tiagox@gmail.com>
2015-04-20 18:13:16 -0300

    Change server.js to fetch advertisers at the beginning.

--- server.js
+++ server.js
@@ -1,40 +1,24 @@
 var fs = require('fs'),
   express = require('express'),
-  app = express();
+  app = express(),
+  advertisers = require('./data/advertisers.js');
 
 app.use(express.static('public'));
 
 app.get('/advertisers', function (req, res) {
-  getAdvertisers(function (advertisers) {
     res.json(advertisers);
-  });
 });
 
 app.get('/advertisers/:id', function (req, res) {
-  var id = req.params.id;
-
-  getAdvertisers(function (advertisers) {
-    var advertiser = advertisers.filter(function (advertiser) {
+  var id = req.params.id,
+    advertiser = advertisers.filter(function (advertiser) {
       return advertiser.id == id;
     }).shift();
 
-    res.send(advertiser);
-  });
+  res.send(advertiser);
 });
 
 var server = app.listen(8080, function () {
   var address = server.address();
-  console.log('Example app listening at http://%s:%s', address.address, address.port);
+  console.log('Advertisers API app listening at http://%s:%s', address.address, address.port);
 });
-
-/**
- * Read the advertisers file from the data folder.
- * @param  {Function} success Callback executed is the file was fetched
- * successfully.
- */
-function getAdvertisers(success){
-  fs.readFile('data/advertisers.json', function (err, data){
-    if (err) throw err;
-    success(JSON.parse(data));
-  });
-}
